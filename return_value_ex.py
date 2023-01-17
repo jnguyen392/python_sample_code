725d5ad5adcbf98fbc2c3c2db93b663d9b698ed6
jnguyen392 <jnguyen392@gmail.com>
2023-01-17 10:16:28 -0600

    Create return_value_ex.py

---
+++ return_value_ex.py
@@ -0,0 +1,11 @@
+"""
+TEST_PROGRAM
+(notes here)
+
+"""
+def format_name(first_n, last_n):
+        full_n = f"{first_n} {last_n}"
+        return full_n.title()
+
+musician = format_name('john', 'nguyen')
+print(musician)
